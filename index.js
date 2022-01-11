6192d8b76b325e9a0591aa5f3616ee405fbb9a60
Sonu Sathyadas <sonusathyadas@hotmail.com>
2022-01-11 16:22:51 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-const serverless = require("serverless-http");
 
-const app = require('./app');
+// var cluster = require('cluster');
 
+// // Code to run if we're in the master process
+// if (cluster.isMaster) { //for node versions > 16.0 use isPrimary
 
-module.exports.handler = serverless(app);
+//     // Count the machine's CPUs
+//     var cpuCount = require('os').cpus().length;
+
+//     // Create a worker for each CPU
+//     for (var i = 0; i < cpuCount; i += 1) {
+//         cluster.fork();
+//     }
+
+//     // Listen for terminating workers
+//     cluster.on('exit', function (worker) {
+
+//         // Replace the terminated workers
+//         console.log('Worker ' + worker.id + ' died :(');
+//         cluster.fork(); //start a new child process when a child exited
+
+//     });
+
+//     // Code to run if we're in a worker process
+// } else {
+//     const PORT = process.env.NODE_ENV === "production" ? 80 : process.env.PORT || 5000;
+//     const app = require('./app');
+//     app.listen(PORT, () => {
+//         console.log(`Express server started on port ${PORT}....`);
+//     });
+// }
+
